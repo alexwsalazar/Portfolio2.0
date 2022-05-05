aed4a0ce0c8c8aafb24815f3ed7a7adc47313265
Alex Salazar <alexwsalazar@gmail.com>
2022-05-05 13:32:27 -0500

    removed basepath

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -12,7 +11,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basepath="/Portfolio2.0" > 
+      <BrowserRouter > 
           <Nav/>
             <Routes>
               <Route path="/About" element={<About/>}/>
