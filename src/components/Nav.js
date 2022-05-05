11dd829e9e66883ff3eb9baece8658e51b0ef5eb
Alex Salazar <alexwsalazar@gmail.com>
2022-05-05 13:38:39 -0500

    trying periods on the link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,16 +7,16 @@ function Nav() {
     <div className="card text-center">
      <ul>
          <li>
-            <Link to="/"> Home </Link>
+            <Link to="./"> Home </Link>
          </li>
          <li>
-            <Link to="/About"> About </Link>
+            <Link to="./About"> About </Link>
          </li>
          <li>
-            <Link to="/Project"> Project </Link>
+            <Link to="./Project"> Project </Link>
          </li>
          <li>
-            <Link to="/Contact"> Contact </Link>
+            <Link to="./Contact"> Contact </Link>
          </li>
      </ul>
     </div>
